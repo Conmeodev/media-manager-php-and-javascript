01152d82e8b60ee2a19307b3a172dbd67b006366
Conmeodev <seluonolai@gmail.com>
2025-08-29 10:26:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,13 @@ link github: https://github.com/Conmeodev
 Liên hệ gmail: linkbattu@gmail.com
 
 */
+$config_file_exists = file_exists('_db_config.php');
+if(!$config_file_exists) {
+	header("location: /install.php");
+	exit();
+}
 $index_file = $_GET['index_file'] ?? "main.php";
 include_once "views/template/head.php";
 include_once "views/template/$index_file";
 include_once "views/template/end.php";
+
